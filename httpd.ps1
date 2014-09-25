f29b5bd1d6201fb7d54050976e21dda5d313a6b5
Roman Kuzmin <nightroman@gmail.com>
2014-09-25 04:55:43 +0100

    Tabs to spaces, to match the original style.

--- httpd.ps1
+++ httpd.ps1
@@ -1,8 +1,8 @@
 ﻿
 param(
-	[Parameter()]$Root = '.',
-	$Port = 8080,
-	$HostName = 'localhost'
+    [Parameter()]$Root = '.',
+    $Port = 8080,
+    $HostName = 'localhost'
 )
 
 $ErrorActionPreference = 'Stop'
@@ -20,29 +20,29 @@ Write-Output ("Start {0} at '$Root'" -f ($listener.Prefixes | Select-Object -Fir
 Write-Output 'Enter Ctrl-Break to stop.'
 
 for() {
-	$context = $listener.GetContext()
-	$url = $context.Request.Url.LocalPath.TrimStart('/')
-	Write-Output "Getting $url"
+    $context = $listener.GetContext()
+    $url = $context.Request.Url.LocalPath.TrimStart('/')
+    Write-Output "Getting $url"
 
-	$res = $context.Response
-	try {
-		# first try root
-		$path = Join-Path $Root $url
-		if (![System.IO.File]::Exists($path)) {
-			# second try here
-			$path = Join-Path $Here $url
-			if (![System.IO.File]::Exists($path)) {
-				Write-Output "Missing $url"
-				$res.StatusCode = 404
-				continue
-			}
-		}
+    $res = $context.Response
+    try {
+        # first try root
+        $path = Join-Path $Root $url
+        if (![System.IO.File]::Exists($path)) {
+            # second try here
+            $path = Join-Path $Here $url
+            if (![System.IO.File]::Exists($path)) {
+                Write-Output "Missing $url"
+                $res.StatusCode = 404
+                continue
+            }
+        }
 
-		Write-Output "Reading $path"
-		$content = [IO.File]::ReadAllBytes($path)
-		$res.OutputStream.Write($content, 0, $content.Length)
-	}
-	finally {
-		$res.Close()
-	}
+        Write-Output "Reading $path"
+        $content = [IO.File]::ReadAllBytes($path)
+        $res.OutputStream.Write($content, 0, $content.Length)
+    }
+    finally {
+        $res.Close()
+    }
 }
